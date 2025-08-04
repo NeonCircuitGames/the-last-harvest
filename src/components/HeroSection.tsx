2600df4e12112581356c87c6e4cc780f1dc701d2
Valeria Belén Cerpa Salas <46113798+vcerpasalas@users.noreply.github.com>
2025-08-03 20:34:00 -0500

    Update HeroSection.tsx

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -56,13 +56,7 @@ export const HeroSection = () => {
             {t('exploreGame')}
           </Button>
         </div>
-        
-        {/* Scroll Indicator */}
-        <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 animate-bounce">
-          <div className="w-6 h-10 border-2 border-neon rounded-full flex justify-center">
-            <div className="w-1 h-3 bg-neon rounded-full mt-2 animate-pulse" />
-          </div>
-        </div>
+    
       </div>
     </section>
   );
